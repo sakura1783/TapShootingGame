7ac752c3c62c424fdaaf7927c91ee552a251ab4b
sakura1783 <sarukaniumai@gmail.com>
2023-07-17 02:00:28 +0900

    手順１５完了

--- Assets/Scripts/DefenceBase.cs
+++ Assets/Scripts/DefenceBase.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 //侵入判定のコライダーは子オブジェクトにアタッチされている。その場合、Rigidbodyをつけることにより、親子関係のあるゲームオブジェクトのコライダーの情報を利用することができるようになる
 [RequireComponent(typeof(Rigidbody2D))]
@@ -8,6 +9,17 @@ public class DefenceBase : MonoBehaviour
 {
     public int durability;
 
+    [SerializeField] private Text txtDurability;
+
+    private int maxDurability;
+
+
+    void Start()
+    {
+        maxDurability = durability;
+
+        DisplayDurability();
+    }
 
     private void OnTriggerEnter2D(Collider2D col)
     {
@@ -30,8 +42,22 @@ public class DefenceBase : MonoBehaviour
     {
         durability -= enemy.attackPower;
 
+        durability = Mathf.Clamp(durability, 0, maxDurability);
+
         Debug.Log($"残りの耐久力：{durability}");
 
+        DisplayDurability();
+
         //TODO 耐久力が0以下になっていないか確認。0以下の場合、ゲームオーバー判定を行う
+    }
+
+    /// <summary>
+    /// 耐久力の表示更新
+    /// </summary>
+    private void DisplayDurability()
+    {
+        txtDurability.text = durability + "/" + maxDurability;
+
+        //TODO ゲージの表示を耐久力の値に合わせて更新
     }
 }
