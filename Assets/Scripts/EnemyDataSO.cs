0be580ba931848960efe973dba70437049746049
sakura1783 <sarukaniumai@gmail.com>
2023-07-17 17:21:53 +0900

    発展２完了

---
+++ Assets/Scripts/EnemyDataSO.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public enum EnemyType
+{
+    Easy,
+    Normal,
+    Elite,
+    Boss,
+}
+
+[CreateAssetMenu(fileName = "EnemyDataSO", menuName = "Create EnemyDataSO")]
+public class EnemyDataSO : ScriptableObject
+{
+    public List<EnemyData> enemyDataList = new List<EnemyData>();
+
+
+    [Serializable]
+    public class EnemyData
+    {
+        public int enemyNo;
+        public int hp;
+        public int attackPoint;
+        public Sprite enemySprite;
+        public EnemyType enemyType;
+    }
+}
